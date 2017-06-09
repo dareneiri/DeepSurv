0a23733cc75231fe4b61473282250a3acf742489
dareneiri <dareneiri@gmail.com>
2017-06-09 15:11:59 -0700

    Create __init__.py

--- deepsurv/__init__.py
+++ deepsurv/__init__.py
@@ -2,5 +2,5 @@ from .deep_surv import DeepSurv
 from .deepsurv_logger import DeepSurvLogger
 from .deepsurv_logger import TensorboardLogger
 from .deep_surv import load_model_from_json
-from .viz import plot_log
+#from .viz import plot_log 
 import datasets
